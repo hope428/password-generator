b5d7f44e0b9cae52cd6cde8cf6e939ab2d75a951
Leon Forsythe <lvforsythe95@gmail.com>
2022-11-08 18:04:22 -0500

    added arrays of characters for passwords

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,10 +6,19 @@ var includeUppercase;
 var includeSpecial;
 var includeNumeric;
 
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var specialCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=',
+'{', '[', '}', ']', '|', '\\', ':', ';', '"', "'", "<", ',', '>', '.', '?', '/']
+var numericCharacters = []
+
 //gets users preferences
-function getUserPreferences() {
+function generatePassword() {
   length = prompt("Enter password length between 8 and 128");
 
+  //validates user selected length
   if (length < 8 || length > 128) {
     alert("Please enter a number between 8 and 128");
     return;
@@ -19,14 +28,33 @@ function getUserPreferences() {
   includeUppercase = confirm("Do you want uppercase letters?");
   includeSpecial = confirm("Do you want special characters?");
   includeNumeric = confirm("Do you want numbers?");
+
+  //validates user input
+  if (
+    !includeLowercase &&
+    !includeUppercase &&
+    !includeSpecial &&
+    !includeNumeric
+  ) {
+    alert("At least one character type must be selected")
+    return;
+  }
+
+
+  //creates password
+  var password = "you passed"
+
+  return password
 }
 
 function writePassword() {
   var password = generatePassword();
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", getUserPreferences);
+generateBtn.addEventListener("click", writePassword);
